221473e01d347c6f8091a2ce387c733ee6f90757
josiasMO <jotamo.jmo10@gmail.com>
2016-11-18 14:33:46 -0200

    Final version before Bluetooth

--- app/src/main/java/com/example/josias/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/josias/tictactoe/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.josias.tictactoe;
 
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,9 +14,12 @@ public class MainActivity extends AppCompatActivity {
     Button newGame;
     Button matrix[] = new Button[9];
     Chronometer timeElapsed;
+    AlertDialog popupEnd;
+
 
     Boolean xTurn, timeRunning;
-    int i;
+    int i, k;
+    int numOfMoves;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,66 +29,72 @@ public class MainActivity extends AppCompatActivity {
         innitiateButtons();
         clearButtons();
 
-        timeElapsed = (Chronometer)findViewById(R.id.gameTime);
+        timeElapsed = (Chronometer) findViewById(R.id.gameTime);
         timeElapsed.setText("00:00");
 
         xTurn = true;
         timeRunning = false;
         handleNewGame();
 
+        popupEnd = new AlertDialog.Builder(this).create();
     }
-    protected void innitiateButtons(){
-        newGame = (Button)findViewById(R.id.buttonNewGame);
-        matrix[0] = (Button)findViewById(R.id.button1x1);
-        matrix[1] = (Button)findViewById(R.id.button1x2);
-        matrix[2] = (Button)findViewById(R.id.button1x3);
-        matrix[3] = (Button)findViewById(R.id.button2x1);
-        matrix[4] = (Button)findViewById(R.id.button2x2);
-        matrix[5] = (Button)findViewById(R.id.button2x3);
-        matrix[6] = (Button)findViewById(R.id.button3x1);
-        matrix[7] = (Button)findViewById(R.id.button3x2);
-        matrix[8] = (Button)findViewById(R.id.button3x3);
+
+    protected void innitiateButtons() {
+        newGame = (Button) findViewById(R.id.buttonNewGame);
+        matrix[0] = (Button) findViewById(R.id.button1x1);
+        matrix[1] = (Button) findViewById(R.id.button1x2);
+        matrix[2] = (Button) findViewById(R.id.button1x3);
+        matrix[3] = (Button) findViewById(R.id.button2x1);
+        matrix[4] = (Button) findViewById(R.id.button2x2);
+        matrix[5] = (Button) findViewById(R.id.button2x3);
+        matrix[6] = (Button) findViewById(R.id.button3x1);
+        matrix[7] = (Button) findViewById(R.id.button3x2);
+        matrix[8] = (Button) findViewById(R.id.button3x3);
 
     }
-    protected void clearButtons(){
 
-        for(i = 0; i < matrix.length;i++){
+    protected void clearButtons() {
+
+        for (i = 0; i < matrix.length; i++) {
             final int j = i;
             matrix[j].setText("");
         }
 
     }
-    protected void handleNewGame(){
+
+    protected void handleNewGame() {
         newGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(timeRunning) {
+                numOfMoves = 0;
+                if (timeRunning) {
                     stopCount();
                     timeRunning = false;
-                }
-                else {
+                } else {
                     clearButtons();
                     startCount();
                     timeRunning = true;
                 }
 
-            handleMatrix();
+                handleMatrix();
 
             }
         });
 
     }
-    protected void startCount(){
+
+    protected void startCount() {
         timeElapsed.setBase(SystemClock.elapsedRealtime());
         timeElapsed.start();
     }
-    protected void stopCount(){
+
+    protected void stopCount() {
         timeElapsed.stop();
     }
 
 
-    protected void handleMatrix(){
-        for(i = 0; i < matrix.length; i++) {
+    protected void handleMatrix() {
+        for (i = 0; i < matrix.length; i++) {
             //Button loop = matrix.get(i);
             final int j = i;
             matrix[i].setOnClickListener(new View.OnClickListener() {
@@ -93,15 +104,67 @@ public class MainActivity extends AppCompatActivity {
                         if (xTurn) {
                             matrix[j].setText("X");
                             xTurn = false;
+                            numOfMoves++;
+                            if (numOfMoves >= 5)
+                                checkEndGame("X");
                         } else if (!xTurn && (matrix[j].getText().toString() != "O") && (matrix[j].getText().toString() != "X")) {
                             matrix[j].setText("O");
                             xTurn = true;
+                            numOfMoves++;
+                            if (numOfMoves >= 5)
+                                checkEndGame("O");
                         }
+                        if (numOfMoves == 9)
+                            noWinner();
+
                     }
                 }
             });
         }
     }
 
+    protected void checkEndGame(String player) {
+        for (k = 0; k < 3; k++) {
+            final int j = k;
+            if ((matrix[j*3].getText().toString() == matrix[(j*3)+1].getText().toString()) && (matrix[(j*3)+1].getText().toString() == matrix[(j*3)+2].getText().toString()) && (matrix[j*3].getText().toString() == player)){
+                popupEnd.setMessage("Player "+player+" is the Winner!!!");
+                popupEnd.setCancelable(true);
+                popupEnd.show();
+                endGame();
+            }
+            else if((matrix[j].getText().toString() == matrix[3+j].getText().toString()) && (matrix[j].getText().toString() == matrix[6+j].getText().toString()) && (matrix[j].getText().toString() == player)){
+                popupEnd.setMessage("Player "+player+" is the Winner!!!");
+                popupEnd.setCancelable(true);
+                popupEnd.show();
+                endGame();
+            }
+            else if((matrix[0].getText().toString() == matrix[4].getText().toString()) && (matrix[0].getText().toString() == matrix[8].getText().toString()) && (matrix[0].getText().toString() == player)){
+                popupEnd.setMessage("Player "+player+" is the Winner!!!");
+                popupEnd.setCancelable(true);
+                popupEnd.show();
+                endGame();
 
+            }
+            else if((matrix[2].getText().toString() == matrix[4].getText().toString()) && (matrix[2].getText().toString() == matrix[6].getText().toString()) &&(matrix[2].getText().toString() ==player)) {
+                popupEnd.setMessage("Player "+player+" is the Winner!!!");
+                popupEnd.setCancelable(true);
+                popupEnd.show();
+                endGame();
+            }
+        }
+
+
+    }
+
+    protected void noWinner() {
+        popupEnd.setMessage("No Winner!!!");
+        popupEnd.setCancelable(true);
+        popupEnd.show();
+        endGame();
+    }
+    protected void endGame() {
+        stopCount();
+        timeRunning = false;
+
+    }
 }
